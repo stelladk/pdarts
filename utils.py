fcbda0ec3c09a105b8489e094dff366bb4672fb8
Stella Douka <35050451+stelladk@users.noreply.github.com>
2026-07-20 10:57:21 +0200

    fix: makedirs non-atomic call

--- utils.py
+++ utils.py
@@ -128,12 +128,11 @@ def drop_path(x, drop_prob):
 
 
 def create_exp_dir(path, scripts_to_save=None):
-  if not os.path.exists(path):
-    os.makedirs(path)
+  os.makedirs(path, exist_ok=True)
   print('Experiment dir : {}'.format(path))
 
   if scripts_to_save is not None:
-    os.mkdir(os.path.join(path, 'scripts'))
+    os.makedirs(os.path.join(path, 'scripts'), exist_ok=True)
     for script in scripts_to_save:
       dst_file = os.path.join(path, 'scripts', os.path.basename(script))
       shutil.copyfile(script, dst_file)
